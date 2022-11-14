5fae1a9e2a97fba204bd1b6a7270ec31d1c0a441
Christophe Prud'homme <christophe.prudhomme@feelpp.org>
2022-11-14 08:05:24 +0100

    up scripts

--- bioprocess/bioprocess.mo
+++ bioprocess/bioprocess.mo
@@ -17,8 +17,8 @@ model Bioprocess "A bioprocess model"
   parameter Real Gamma_VCC_asn=1.75 "k_asn";
   parameter Real ms=0.023 "ms";
   parameter Real qp=0.00108 "qp" ;
-  parameter Real c_asn_feed=0.0001 "c_asn_feed";
-  parameter Real c_glc_feed=0.0001 "c_glc_feed";
+  parameter Real c_asn_feed=0.001 "c_asn_feed";
+  parameter Real c_glc_feed=0.001 "c_glc_feed";
   parameter Real c_glc_init=0.0001 "c_glc_init";
   parameter Real c_asn_init=0.0001 "c_asn_init";
   parameter Real c_p_init=0.0001 "c_p_init";
--- bioprocess/bioprocess.py
+++ bioprocess/bioprocess.py
@@ -1,30 +1,38 @@
-
 import numpy as np
 from fmpy import *
-fmu = 'bioprocess.fmu'
+import os
+fmu = os.getcwd() + '/bioprocess.fmu'
 #dump(fmu)
 
 start_values={
         'VCC_init': 1., 
-        'c_glc_init': 1, 
+        'c_glc_init': 3, 
         'c_asn_init': 1.0,
-        'DCC_init': 0, 
+        'DCC_init': 0.1, 
         'c_p_init': 0, 
         'mu_max': 20,
         'k_d': 0.002,
         'ms': 0.023,
         'k_asn': 1.91,
         'Gamma_VCC_glc': 0.39,
-        'Gamma_VCC_asn': 1.75,
+        'Gamma_VCC_asn': 1.75,    
         'qp': 1e-3,
         }
 result = simulate_fmu(filename=fmu,start_values=start_values, stop_time=15,fmi_call_logger=print)
-print(result)
+#print(result)
 
 import plotly.io as pio
-pio.renderers.default = "notebook"
+#pio.renderers.default = "notebook"
 #pio.renderers.default = "vscode"
 
 import plotly.graph_objects as go
 fig = go.Figure()
-fig.add_trace(go.Scatter3d(x=result['time'], y=result['VCC'], mode='lines'))
+fig.add_trace(go.Scatter(x=result['time'], y=result['VCC'], mode='lines', name='VCC'))
+fig.add_trace(go.Scatter(x=result['time'], y=result['DCC'], mode='lines', name='DCC'))
+fig.add_trace(go.Scatter(x=result['time'], y=result['c_glc'], mode='lines', name='c_glc'))
+fig.add_trace(go.Scatter(x=result['time'], y=result['c_asn'], mode='lines', name='c_asn'))
+fig.add_trace(go.Scatter(x=result['time'], y=result['cp'], mode='lines', name='cp'))
+fig.update_layout(legend_title_text = "Bio Process")
+fig.update_xaxes(title_text="time (s)")
+
+fig.show()
